91f8ed6f120378633e19b56c865beda29c129b31
vaibhav kohli <vaibhav.khl@gmail.com>
2015-06-08 02:45:52 +0530

    crawling algorithm added, exporting links

--- lib/crawl.rb
+++ lib/crawl.rb
@@ -5,6 +5,7 @@ class Crawl
     link_crawler = LinkCrawler.new(base_url, max_links)
     link_crawler.crawl_links
     puts "Done crawling"
+    link_crawler.export_links
   end
 end
 
--- lib/link_crawler.rb
+++ lib/link_crawler.rb
@@ -1,28 +1,83 @@
 require 'mechanize'
+#require 'fileutils'
 
 class LinkCrawler
   attr_reader :base_url
   attr_reader :max_links
+  attr_accessor :link_repository
+  attr_accessor :links_to_crawl
+  attr_accessor :links_visited
 
   def initialize(base_url, max_links)
     @base_url = base_url
     @max_links = max_links
+    @link_repository = []
+    @links_to_crawl = []
+    @links_visited = []
+    @agent = Mechanize.new
   end
 
   def crawl_links
-	  agent = Mechanize.new
-	  page = agent.get(base_url)
+    links_to_crawl.push(base_url)
+    puts "starting..."
+    start_crawling
+  end
 
-	  page.links.each do |link|
-		  puts link.uri()
-		  #@link_repository.push(link)
-		  # if link.uri().class == URI::HTTPS
-			 #  begin
-		  #     get_links_from_this_link(link)
-		  #   rescue
-		  # 	  next
-		  # end
-		end
-	end
+
+  def start_crawling
+    while links_to_crawl.count > 0
+      if max_crawl?
+
+        next_link = get_next_link
+        puts next_link
+
+        begin
+          page = @agent.get(next_link)
+          links_visited.push(next_link)
+
+          page.links.each do |link|
+            link_repository.push(link.uri())
+            links_to_crawl.push(link.uri())
+          end
+
+        rescue
+          next
+        end
+
+      end
+    end
+  end
+
+  def get_next_link
+    links_to_crawl.shift
+  end
+
+  def max_crawl?
+    return true unless max_links
+    return true if links_visited.count < max_links
+    puts "max limit of #{max_links} reached, stopping..."
+    links_to_crawl.clear
+    false
+  end
+
+  def export_dir
+    File.expand_path "#{File.dirname(__FILE__)}/../data/"
+  end
+
+  def export_filename
+    "#{export_dir}/#{base_url}.txt"
+  end
+
+  def write_links_to_file
+    #FileUtils.mkdir_p export_dir
+    File.open('links.txt', "w+") do |f|
+      f.puts(link_repository)
+    end
+  end
+
+  def export_links
+    puts "exporting links repository to links.txt file."
+    write_links_to_file
+  end
 
 end
