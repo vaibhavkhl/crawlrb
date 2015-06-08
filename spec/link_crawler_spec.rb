b1fe505d887a152c78f090564f11864a8e121842
vaibhav kohli <vaibhav.khl@gmail.com>
2015-06-09 00:32:00 +0530

    code formatting
    sublime: converting indentation to spaces

--- spec/link_crawler_spec.rb
+++ spec/link_crawler_spec.rb
@@ -2,23 +2,23 @@ require 'spec_helper'
 require 'link_crawler'
 
 RSpec.describe LinkCrawler do
-	before(:each) do
+  before(:each) do
     @link_crawler = LinkCrawler.new('http://vaibhavkhl.in', 2)
   end
 
-	it "should have base_url and max_links accessors" do
+  it "should have base_url and max_links accessors" do
     @link_crawler.base_url.should eql 'http://vaibhavkhl.in'
     @link_crawler.max_links.should eql 2
   end
 
   it "should have links_repository, links_visited & links_to_crawl accessors" do
-  	@link_crawler.link_repository.should eql []
-  	@link_crawler.links_visited.should eql []
-  	@link_crawler.links_to_crawl.should eql []
+    @link_crawler.link_repository.should eql []
+    @link_crawler.links_visited.should eql []
+    @link_crawler.links_to_crawl.should eql []
   end
 
-	it "should crawl base url and add links to link repository" do
-		@link_crawler.crawl_links
+  it "should crawl base url and add links to link repository" do
+    @link_crawler.crawl_links
     @link_crawler.link_repository.length.should eql 18
   end
 
